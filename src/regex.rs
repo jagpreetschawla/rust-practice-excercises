076bad7c86d50917f783e3efa85c3e3e8651d927
Chawla, Jagpreet Singh <jagpreeetschawla+git@gmail.com>
2022-08-14 00:06:06 -0700

    pattern matching solution

--- src/regex.rs
+++ src/regex.rs
@@ -1,12 +1,119 @@
 use std::collections::HashSet;
 
-#[derive(Debug)]
-enum PatternType {
-    Set{one_of: HashSet<u8>, min_count: u8, max_count: u8},
-    Range{between: (u8, u8), min_count: u8, max_count: u8}
+enum MatchCountStatus {
+    Under,
+    Over,
+    MaxMatch,
+    InRange,
 }
 
-pub struct Pattern(Vec<PatternType>);
+impl MatchCountStatus {
+    fn get_status(min: &u8, max: &u8, to_check: &u8) -> Self {
+        if to_check < min {
+            Self::Under
+        } else if to_check > max {
+            Self::Over
+        } else if to_check == max {
+            Self::MaxMatch
+        } else {
+            Self::InRange
+        }
+    }
+}
+
+#[derive(Debug)]
+enum SubPattern {
+    Set {
+        one_of: HashSet<u8>,
+        min_count: u8,
+        max_count: u8,
+    },
+    Range {
+        between: (u8, u8),
+        min_count: u8,
+        max_count: u8,
+    },
+}
+
+impl SubPattern {
+    fn matches(&self, c: u8) -> bool {
+        match self {
+            SubPattern::Set { one_of, .. } => one_of.contains(&c),
+            SubPattern::Range { between, .. } => (between.0..=between.1).contains(&c),
+        }
+    }
+
+    fn check_count(&self, count: u8) -> MatchCountStatus {
+        match self {
+            SubPattern::Set {
+                min_count,
+                max_count,
+                ..
+            } => MatchCountStatus::get_status(min_count, max_count, &count),
+            SubPattern::Range {
+                min_count,
+                max_count,
+                ..
+            } => MatchCountStatus::get_status(min_count, max_count, &count),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum StateStatus<'a> {
+    PendingMinimumMatches(PatternState<'a>),
+    MatchInRange(PatternState<'a>),
+    ExhaustedMatchLimit(Option<PatternState<'a>>),
+    MatchFailed,
+}
+
+#[derive(Debug)]
+struct PatternState<'a> {
+    pattern: &'a Pattern,
+    subpattern_index: usize,
+    matched_count: u8,
+}
+
+impl<'a> PatternState<'a> {
+    fn root_state(pattern: &'a Pattern) -> Self {
+        Self {
+            pattern,
+            subpattern_index: 0,
+            matched_count: 0,
+        }
+    }
+
+    fn next_sub_pattern(&self) -> Option<Self> {
+        let next_index = self.subpattern_index + 1;
+        if next_index == self.pattern.0.len() {
+            None
+        } else {
+            Some(Self {
+                pattern: self.pattern,
+                subpattern_index: next_index,
+                matched_count: 0,
+            })
+        }
+    }
+
+    fn update(mut self, c: u8) -> StateStatus<'a> {
+        let subpattern = &self.pattern.0[self.subpattern_index];
+        if subpattern.matches(c) {
+            self.matched_count += 1;
+            match subpattern.check_count(self.matched_count) {
+                MatchCountStatus::Under => StateStatus::PendingMinimumMatches(self),
+                MatchCountStatus::MaxMatch => StateStatus::ExhaustedMatchLimit(self.next_sub_pattern()),
+                MatchCountStatus::Over => panic!("code should never reach here, possible bug"),
+                MatchCountStatus::InRange => StateStatus::MatchInRange(self),
+            }
+        } else {
+            StateStatus::MatchFailed
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Pattern(Vec<SubPattern>);
 
 impl Pattern {
     pub fn new(pattern: &str) -> Self {
@@ -16,10 +123,24 @@ impl Pattern {
         // extracted common code used within this function into an internal function for reuse.
         fn extract_counts(mut pattern: &[u8], num_start_pos: usize) -> (u8, u8, usize) {
             pattern = &pattern[num_start_pos..];
-            let num_sep_pos = pattern.iter().position(|x| *x == b',').expect("expected comma separator for number range, invalid pattern");
-            let brack_close_pos = pattern[(num_sep_pos+1)..].iter().position(|x| *x == b'}').expect("expected } at end, invalid pattern") + num_sep_pos+1;
-            let min_count = std::str::from_utf8(&pattern[..num_sep_pos]).unwrap().parse().expect("expected valid number");
-            let max_count = std::str::from_utf8(&pattern[(num_sep_pos+1)..brack_close_pos]).unwrap().parse().expect("expected valid number");
+            let num_sep_pos = pattern
+                .iter()
+                .position(|x| *x == b',')
+                .expect("expected comma separator for number range, invalid pattern");
+            let brack_close_pos = pattern[(num_sep_pos + 1)..]
+                .iter()
+                .position(|x| *x == b'}')
+                .expect("expected } at end, invalid pattern")
+                + num_sep_pos
+                + 1;
+            let min_count = std::str::from_utf8(&pattern[..num_sep_pos])
+                .unwrap()
+                .parse()
+                .expect("expected valid number");
+            let max_count = std::str::from_utf8(&pattern[(num_sep_pos + 1)..brack_close_pos])
+                .unwrap()
+                .parse()
+                .expect("expected valid number");
             (min_count, max_count, num_start_pos + brack_close_pos)
         }
 
@@ -30,8 +151,12 @@ impl Pattern {
                 // range pattern
                 assert!(pattern[4] == b']' && pattern[5] == b'{', "invalid pattern");
                 let (min_count, max_count, end_pos) = extract_counts(pattern, 6);
-                patterns.push(PatternType::Range { between: (pattern[1], pattern[3]), min_count, max_count });
-                &pattern[(end_pos+1)..]
+                patterns.push(SubPattern::Range {
+                    between: (pattern[1], pattern[3]),
+                    min_count,
+                    max_count,
+                });
+                &pattern[(end_pos + 1)..]
             } else {
                 // set pattern
                 assert!(pattern[2] == b',' || pattern[2] == b']', "invalid pattern");
@@ -41,21 +166,86 @@ impl Pattern {
                 let num_start_pos = loop {
                     let (_, c) = p_iter.next().expect("pattern unexpectedly ended");
                     char_set.insert(*c);
-                    let (ni, nc) = p_iter.next().filter(|(_, x)| **x == b',' || **x == b']').expect("expected , or ] after char, invalid pattern");
+                    let (ni, nc) = p_iter
+                        .next()
+                        .filter(|(_, x)| **x == b',' || **x == b']')
+                        .expect("expected , or ] after char, invalid pattern");
                     if *nc == b']' {
-                        break ni+2;
+                        break ni + 2;
                     }
                 };
                 let (min_count, max_count, end_pos) = extract_counts(pattern, num_start_pos);
-                patterns.push(PatternType::Set { one_of: char_set, min_count, max_count });
-                &pattern[(end_pos+1)..]
+                patterns.push(SubPattern::Set {
+                    one_of: char_set,
+                    min_count,
+                    max_count,
+                });
+                &pattern[(end_pos + 1)..]
             }
         }
         Pattern(patterns)
     }
 
     pub fn find_matches<'s, 'i>(&'s self, inp: &'i str) -> Vec<&'i str> {
-        todo!("implement this")
+        let mut matches = Vec::new();
+
+        // extracted common code used within this function into an internal function for reuse.
+        fn check_status_n_update_next<'a>(
+            s: PatternState<'a>,
+            c: u8,
+            next_to_check: &mut Vec<PatternState<'a>>,
+        ) -> bool {
+            let mut pattern_completed = false;
+            let next_to_consider = match s.update(c) {
+                StateStatus::PendingMinimumMatches(s) => {
+                    next_to_check.push(s);
+                    None
+                },
+                StateStatus::MatchInRange(s) => {
+                    let nxt = s.next_sub_pattern();
+                    next_to_check.push(s);
+                    Some(nxt)
+                },
+                StateStatus::ExhaustedMatchLimit(next) => Some(next),
+                StateStatus::MatchFailed => None,
+            };
+            
+            if let Some(nxt) = next_to_consider {
+                match nxt {
+                    Some(ns) => {
+                        next_to_check.push(ns);
+                    }
+                    None => pattern_completed = true,
+                }   
+            }
+            pattern_completed
+        }
+
+        let mut start_idx = 0;
+        let inp_bytes = inp.as_bytes();
+        while start_idx < inp_bytes.len() {
+            let mut match_end_idx = None;
+            let mut active_states: Vec<PatternState> =
+                vec![PatternState::root_state(self)];
+            for (i, c) in inp_bytes.iter().enumerate().skip(start_idx) {
+                let mut new_states = Vec::new();
+                for s in active_states.into_iter() {
+                    if check_status_n_update_next(s, *c, &mut new_states) {
+                        match_end_idx = Some(i);
+                    }
+                }
+                active_states = new_states;
+                if active_states.is_empty() {
+                    break;
+                }
+            }
+            if let Some(end_idx) = match_end_idx {
+                matches.push(&inp[start_idx..=end_idx]);
+                start_idx = end_idx;
+            }
+            start_idx += 1;
+        }
+        matches
     }
 }
 
